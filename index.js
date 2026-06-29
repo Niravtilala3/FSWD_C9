1f97ce3b8261a5a4f12a699f6b9205fc906bb8e3
Nirav Tilala <niravtilala3@gmail.com>
2026-06-29 09:58:08 +0530

    Change view engine from Pug to EJS and add initial EJS template

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const userRoute = require('./routes/userRoute');
 const empRoute = require('./routes/empRoute');
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index',

a38741765f12e157886b2214221c00588d8a4117
Nirav Tilala <niravtilala3@gmail.com>
2026-06-29 08:37:57 +0530

    Refactor routes and enhance index view with dynamic data

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
-const userRoute = require('./route/userRoute');
-const empRoute = require('./route/empRoute');
+const userRoute = require('./routes/userRoute');
+const empRoute = require('./routes/empRoute');
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index',
+    {
+      title: 'Hey', 
+      message: 'Hello there!', 
+      list:[{name:'raj'},{name:'john'},{name:'jane'}] 
+    });
   //res.send('Hello World!');
 });
 
@@ -19,5 +24,5 @@ app.use('/user', userRoute);
 app.use('/emp', empRoute);
 
 app.listen(3000,()=>{
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port http://localhost:3000');
 });

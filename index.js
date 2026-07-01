97e5f8366bae45726e621cb9b7bdecdd3f4dce3c
Nirav Tilala <niravtilala3@gmail.com>
2026-07-01 10:21:12 +0530

    Switch view engine from EJS to Pug, add 404 error handling, and create 404 template

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const app = express();
 const userRoute = require('./routes/userRoute');
 const empRoute = require('./routes/empRoute');
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+
+  console.log('req.hostname', req.hostname);
+  console.log('req.ip', req.ip);
+  console.log('req.path', req.path);
+  console.log('req.method', req.method);
+  console.log('req.protocol', req.protocol);
+
   res.render('index',
     {
       title: 'Hey', 
@@ -25,6 +32,26 @@ app.post('/', (req, res) => {
 app.use('/user', userRoute);
 app.use('/emp', empRoute);
 
-app.listen(3000,()=>{
-  console.log('Server is running on port http://localhost:3000');
+
+
+// Error handling middleware
+
+app.get('/error', (req, res) => {
+  throw new Error('BROKEN'); // Express will catch this on its own.
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  //
+  res.status(500).send('Something broke!');
+});
+
+// 404 
+app.use((req, res, next) => {
+  res.status(404).render('404', { title: '404 Not Found' });
+});
+
+
+app.listen(3300,()=>{
+  console.log('Server is running on port http://localhost:3300');
 });
